40fad2c9d4c328a7e9d612ab50fb17677ff67167
robomamcak-hub <robo.mamcak@gmail.com>
2026-08-19 19:56:00 +0200

    MAVEX JUNIOR – nový dizajn

--- sw.js
+++ sw.js
@@ -1,5 +1,8 @@
-const CACHE="mavex-junior-3d-v2";
-const ASSETS=["./","./index.html","./manifest.json","./logo.png","./icon-192.png","./icon-512.png"];
+const CACHE="mavex-junior-wau-v1";
+const ASSETS=["./","./index.html","./manifest.json","./wau-design.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch",e=>{if(new URL(e.request.url).origin===location.origin)e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+self.addEventListener("fetch",e=>{
+  if(new URL(e.request.url).origin===location.origin)
+    e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request)));
+});
